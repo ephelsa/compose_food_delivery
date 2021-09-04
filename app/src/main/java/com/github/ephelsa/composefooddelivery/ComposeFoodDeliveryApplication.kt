f04bd9337948d8ea66ed3a2057cb353370b20fea
Leonardo <xephelsax@gmail.com>
2021-09-04 18:04:52 -0500

    add and initialize hilt

---
+++ app/src/main/java/com/github/ephelsa/composefooddelivery/ComposeFoodDeliveryApplication.kt
@@ -0,0 +1,7 @@
+package com.github.ephelsa.composefooddelivery
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class ComposeFoodDeliveryApplication : Application()
