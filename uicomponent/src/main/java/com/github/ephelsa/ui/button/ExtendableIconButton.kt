28d4df1288a6790ea3bd3ff89396ad3b3d2b9537
Leonardo <xephelsax@gmail.com>
2021-09-05 00:01:43 -0500

    mod: separate in own composable functions and add a cover icon

--- uicomponent/src/main/java/com/github/ephelsa/ui/button/ExtendableIconButton.kt
+++ uicomponent/src/main/java/com/github/ephelsa/ui/button/ExtendableIconButton.kt
@@ -2,16 +2,20 @@ package com.github.ephelsa.ui.button
 
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.expandIn
 import androidx.compose.animation.expandVertically
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
+import androidx.compose.animation.shrinkOut
 import androidx.compose.animation.shrinkVertically
 import androidx.compose.animation.slideInVertically
 import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -21,18 +25,22 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Home
-import androidx.compose.material.ripple.rememberRipple
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.semantics.Role
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.github.ephelsa.ui.theme.ExtraSmallSpacing
+import com.github.ephelsa.ui.theme.MediumSpacing
 import com.github.ephelsa.ui.theme.PinkSwan
 
 @ExperimentalAnimationApi
@@ -45,48 +53,98 @@ fun ExtendableIconButton(
     onClick: () -> Unit
 ) {
     val (extended, setExtend) = remember { mutableStateOf(isExtended) }
-    val width = text.length * MaterialTheme.typography.body1.fontSize.value
 
     Column(
         modifier = Modifier
-            .width(width.dp),
+            .width((ExpandableIcon.value * 1.5).dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+
+        IconButton(imageVector, text, extended, iconColor) {
+            setExtend(!extended)
+            onClick()
+        }
+        Spacer(Modifier.height(ExtraSmallSpacing))
+        ExtendableText(extended, text)
+    }
+}
+
+@ExperimentalAnimationApi
+@Composable
+private fun IconButton(
+    imageVector: ImageVector,
+    contentDescription: String,
+    isExtended: Boolean,
+    iconColor: Color = PinkSwan,
+    onClick: () -> Unit
+) {
+    Box(
+        contentAlignment = Alignment.Center
+    ) {
+        AnimatedVisibility(
+            visible = isExtended,
+            enter = expandIn(),
+            exit = shrinkOut()
+        ) {
+            Spacer(
+                modifier = Modifier
+                    .size(ExpandableIcon)
+                    .graphicsLayer(
+                        scaleX = CoverScale,
+                        scaleY = CoverScale
+                    )
+                    .clip(CircleShape)
+                    .shadow(
+                        elevation = 4.dp,
+                        shape = CircleShape
+                    )
+                    .background(
+                        color = MaterialTheme.colors.surface
+                    )
+            )
+        }
+
         Icon(
             imageVector = imageVector,
-            contentDescription = text,
+            contentDescription = contentDescription,
             modifier = Modifier
-                .size(35.dp)
+                .size(ExpandableIcon)
                 .clip(CircleShape)
                 .clickable(
-                    interactionSource = remember { MutableInteractionSource() },
-                    indication = rememberRipple(bounded = true, radius = 30.dp),
-                    onClick = {
-                        setExtend(!extended)
-                        onClick()
-                    },
+                    onClick = onClick,
                     role = Role.Button
                 )
                 .background(
-                    color = if (extended) MaterialTheme.colors.primary else Color.Transparent,
+                    color = if (isExtended) MaterialTheme.colors.primary else Color.Transparent,
                 )
-                .padding(6.dp),
-            tint = if (extended) MaterialTheme.colors.surface else iconColor
+                .padding(MediumSpacing),
+            tint = if (isExtended) MaterialTheme.colors.surface else iconColor
         )
-
-        AnimatedVisibility(
-            visible = extended,
-            enter = slideInVertically() + expandVertically() + fadeIn(),
-            exit = slideOutVertically() + shrinkVertically() + fadeOut()
-        ) {
-            Text(
-                text = text,
-                style = MaterialTheme.typography.button,
-                color = MaterialTheme.colors.primary
-            )
-        }
     }
 }
+
+@ExperimentalAnimationApi
+@Composable
+private fun ExtendableText(
+    isExtended: Boolean,
+    text: String
+) {
+    AnimatedVisibility(
+        visible = isExtended,
+        enter = slideInVertically() + expandVertically() + fadeIn(),
+        exit = slideOutVertically() + shrinkVertically() + fadeOut()
+    ) {
+        Text(
+            text = text,
+            style = MaterialTheme.typography.button,
+            color = MaterialTheme.colors.primary,
+            textAlign = TextAlign.Center
+        )
+    }
+}
+
+private val ExpandableIcon = 40.dp
+private const val CoverScale = 1.4f
 
 @ExperimentalAnimationApi
 @Preview
