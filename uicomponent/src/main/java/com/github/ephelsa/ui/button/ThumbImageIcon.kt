b8a106b7777e209a9e2e1bc94798b66fe9cb7d6b
Leonardo <xephelsax@gmail.com>
2021-09-04 20:41:57 -0500

    add `ThumbImageIcon`

---
+++ uicomponent/src/main/java/com/github/ephelsa/ui/button/ThumbImageIcon.kt
@@ -0,0 +1,55 @@
+package com.github.ephelsa.ui.button
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun ThumbImageIcon(
+    imageVector: ImageVector,
+    contentDescription: String?,
+    onClick: () -> Unit,
+) {
+    Image(
+        imageVector = imageVector,
+        contentDescription = contentDescription,
+        modifier = Modifier
+            .clip(CircleShape)
+            .size(40.dp)
+            .border(
+                width = 2.dp,
+                color = MaterialTheme.colors.primary,
+                shape = CircleShape
+            )
+            .background(
+                color = MaterialTheme.colors.surface,
+            )
+            .clickable(
+                onClick = onClick
+            )
+    )
+}
+
+@Preview
+@Composable
+internal fun ThumbImageIconPreview() {
+    ThumbImageIcon(
+        imageVector = Icons.Filled.Person,
+        contentDescription = "Profile"
+    ) {
+
+    }
+}
+
