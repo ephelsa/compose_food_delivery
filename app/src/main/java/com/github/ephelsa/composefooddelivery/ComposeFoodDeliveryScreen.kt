ca64b8773b84c5c439297893af15907d6f9d222b
Leonardo <xephelsax@gmail.com>
2021-09-05 00:11:52 -0500

    add Home, Favorites, Location, Shopping Cart, Profile and Details routes

--- app/src/main/java/com/github/ephelsa/composefooddelivery/ComposeFoodDeliveryScreen.kt
+++ app/src/main/java/com/github/ephelsa/composefooddelivery/ComposeFoodDeliveryScreen.kt
@@ -1,15 +1,68 @@
 package com.github.ephelsa.composefooddelivery
 
+import androidx.annotation.StringRes
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Favorite
+import androidx.compose.material.icons.rounded.Home
+import androidx.compose.material.icons.rounded.LocationOn
+import androidx.compose.material.icons.rounded.Person
+import androidx.compose.material.icons.rounded.ShoppingCart
+import androidx.compose.ui.graphics.vector.ImageVector
+
 /**
  * This class contains all the screen's routes for the application!
  */
-enum class ComposeFoodDeliveryScreen {
-    Home,
-    Details;
+enum class ComposeFoodDeliveryScreen(
+    val toolbar: Boolean,
+    val bottomBar: Boolean,
+    val isBottomBarOption: Boolean,
+    val icon: ImageVector? = null,
+    @StringRes val strRes: Int? = null
+) {
+    Home(
+        toolbar = true,
+        bottomBar = true,
+        isBottomBarOption = true,
+        icon = Icons.Rounded.Home,
+        strRes = R.string.bottomBarOption_home
+    ),
+    Favorites(
+        toolbar = true,
+        bottomBar = true,
+        isBottomBarOption = true,
+        icon = Icons.Rounded.Favorite,
+        strRes = R.string.bottomBarOption_favorites
+    ),
+    Location(
+        toolbar = true,
+        bottomBar = true,
+        isBottomBarOption = true,
+        icon = Icons.Rounded.LocationOn,
+        strRes = R.string.bottomBarOption_location
+    ),
+    ShoppingCart(
+        toolbar = true,
+        bottomBar = true,
+        isBottomBarOption = true,
+        icon = Icons.Rounded.ShoppingCart,
+        strRes = R.string.bottomBarOption_shoppingCart
+    ),
+    Profile(
+        toolbar = true,
+        bottomBar = true,
+        isBottomBarOption = true,
+        icon = Icons.Rounded.Person,
+        strRes = R.string.bottomBarOption_profile
+    ),
+    Details(
+        toolbar = false,
+        bottomBar = false,
+        isBottomBarOption = false,
+    );
 
     companion object {
         fun fromRoute(route: String?): ComposeFoodDeliveryScreen = when (route?.substringBefore("/")) {
-            Home.name -> Home
+            Home.name, null -> Home
             Details.name -> Details
             else -> throw IllegalArgumentException("Route $route is not recognized!")
         }
