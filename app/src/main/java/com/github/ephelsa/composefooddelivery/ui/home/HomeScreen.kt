ccdae6434df590054f16d0124dde171274a62bc5
Leonardo <xephelsax@gmail.com>
2021-09-05 16:02:04 -0500

    add category cards and recommended cards

--- app/src/main/java/com/github/ephelsa/composefooddelivery/ui/home/HomeScreen.kt
+++ app/src/main/java/com/github/ephelsa/composefooddelivery/ui/home/HomeScreen.kt
@@ -1,5 +1,6 @@
 package com.github.ephelsa.composefooddelivery.ui.home
 
+import android.widget.Toast
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -13,17 +14,24 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Share
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.font.FontWeight
 import com.github.ephelsa.composefooddelivery.R
 import com.github.ephelsa.ui.button.SimpleIconButton
-import com.github.ephelsa.ui.theme.ExtraLargeSpacing
-import com.github.ephelsa.ui.theme.MediumSpacing1
+import com.github.ephelsa.ui.card.CategoryCard
+import com.github.ephelsa.ui.card.RecommendedCard
+import com.github.ephelsa.ui.theme.ExtraHugeSpacing
+import com.github.ephelsa.ui.theme.HugeSpacing
+import com.github.ephelsa.ui.theme.LargeSpacing
 
 @Composable
 fun HomeBody() {
@@ -31,16 +39,16 @@ fun HomeBody() {
         modifier = Modifier
             .fillMaxSize()
             .padding(
-                horizontal = ExtraLargeSpacing
+                horizontal = HugeSpacing
             )
             .verticalScroll(rememberScrollState())
     ) {
         Title()
-        Spacer(Modifier.height(ExtraLargeSpacing))
+        Spacer(Modifier.height(ExtraHugeSpacing))
         Search()
-        Spacer(Modifier.height(ExtraLargeSpacing))
+        Spacer(Modifier.height(ExtraHugeSpacing))
         CategorySection()
-        Spacer(Modifier.height(ExtraLargeSpacing))
+        Spacer(Modifier.height(ExtraHugeSpacing))
         RecommendedSection()
     }
 }
@@ -88,13 +96,19 @@ private fun CategorySection() {
             style = MaterialTheme.typography.h5
         )
 
-        Spacer(Modifier.height(ExtraLargeSpacing))
+        Spacer(Modifier.height(ExtraHugeSpacing))
 
         LazyRow(
-            horizontalArrangement = Arrangement.spacedBy(MediumSpacing1)
+            horizontalArrangement = Arrangement.spacedBy(LargeSpacing)
         ) {
             items(10) {
-                Text(text = it.toString())
+                CategoryCard(
+                    text = "Sandwich",
+                    imageVector = Icons.Filled.Email,
+                    isSelected = true
+                ) {
+
+                }
             }
         }
     }
@@ -102,19 +116,33 @@ private fun CategorySection() {
 
 @Composable
 private fun RecommendedSection() {
+    val context = LocalContext.current
+
     Column {
         Text(
             text = stringResource(R.string.sectionLabel_recommended),
             style = MaterialTheme.typography.h6
         )
 
-        Spacer(Modifier.height(ExtraLargeSpacing))
+        Spacer(Modifier.height(ExtraHugeSpacing))
 
         LazyRow(
-            horizontalArrangement = Arrangement.spacedBy(MediumSpacing1)
+            horizontalArrangement = Arrangement.spacedBy(LargeSpacing)
         ) {
             items(10) {
-                Text(text = it.toString())
+                RecommendedCard(
+                    imageVector = Icons.Filled.Share,
+                    name = "Sandwich",
+                    label = "Starting From",
+                    price = 15.50,
+                    isAvailable = true,
+                    onDetails = {
+                        Toast.makeText(context, "Details", Toast.LENGTH_SHORT).show()
+                    },
+                    onAdd = {
+                        Toast.makeText(context, "Add", Toast.LENGTH_SHORT).show()
+                    }
+                )
             }
         }
     }
--- uicomponent/src/main/java/com/github/ephelsa/ui/theme/Space.kt
+++ uicomponent/src/main/java/com/github/ephelsa/ui/theme/Space.kt
@@ -8,3 +8,5 @@ val MediumSpacing = 6.dp
 val MediumSpacing1 = 8.dp
 val LargeSpacing = 12.dp
 val ExtraLargeSpacing = 16.dp
+val HugeSpacing = 18.dp
+val ExtraHugeSpacing = 20.dp
