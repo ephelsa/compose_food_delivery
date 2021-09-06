55301e69674662fba483c4b7abfb831afaf65565
Leonardo <xephelsax@gmail.com>
2021-09-05 23:07:19 -0500

    add `ThumbImageIcon` with Painter and ImageVector to use with Coil

--- uicomponent/src/main/java/com/github/ephelsa/ui/button/ThumbImageIcon.kt
+++ uicomponent/src/main/java/com/github/ephelsa/ui/button/ThumbImageIcon.kt
@@ -12,7 +12,9 @@ import androidx.compose.material.icons.filled.Person
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
@@ -20,10 +22,17 @@ import androidx.compose.ui.unit.dp
 fun ThumbImageIcon(
     imageVector: ImageVector,
     contentDescription: String?,
+    onClick: () -> Unit
+) = ThumbImageIcon(rememberVectorPainter(imageVector), contentDescription, onClick)
+
+@Composable
+fun ThumbImageIcon(
+    painter: Painter,
+    contentDescription: String?,
     onClick: () -> Unit,
 ) {
     Image(
-        imageVector = imageVector,
+        painter = painter,
         contentDescription = contentDescription,
         modifier = Modifier
             .clip(CircleShape)
