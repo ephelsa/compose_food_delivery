c6a807045c4aa0468e2bcc3385b9abe26bda8799
Leonardo <xephelsax@gmail.com>
2021-09-05 23:29:18 -0500

    add animation in loader thumbnail image

--- app/src/main/java/com/github/ephelsa/composefooddelivery/ui/extras/Toolbar.kt
+++ app/src/main/java/com/github/ephelsa/composefooddelivery/ui/extras/Toolbar.kt
@@ -1,9 +1,13 @@
 package com.github.ephelsa.composefooddelivery.ui.extras
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.foundation.layout.Box
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.unit.dp
@@ -16,6 +20,7 @@ import com.github.ephelsa.ui.button.SimpleIconButton
 import com.github.ephelsa.ui.button.ThumbImageIcon
 import com.github.ephelsa.ui.toolbar.ApplicationToolbar
 
+@ExperimentalAnimationApi
 @ExperimentalCoilApi
 @Composable
 fun UserFoodDeliveryToolbar(
@@ -24,21 +29,14 @@ fun UserFoodDeliveryToolbar(
     onSettingsClick: () -> Unit
 ) {
     val thumbURL by viewModel.onUserThumbnail.collectAsState()
+    val painter = rememberImagePainter(
+        data = thumbURL
+    )
     val shouldLoad by viewModel.onLoadUserThumbnail.collectAsState()
 
     ApplicationToolbar(
         leftItem = {
-            if (shouldLoad) {
-                CircularProgressIndicator()
-            } else {
-                ThumbImageIcon(
-                    painter = rememberImagePainter(
-                        data = thumbURL
-                    ),
-                    contentDescription = "Profile",
-                    onClick = onProfileClick
-                )
-            }
+            UserThumbnail(!shouldLoad, painter, onProfileClick)
         },
         rightItem = {
             SimpleIconButton(
@@ -50,4 +48,30 @@ fun UserFoodDeliveryToolbar(
             )
         }
     )
+}
+
+@ExperimentalAnimationApi
+@Composable
+fun UserThumbnail(
+    isVisible: Boolean,
+    painter: Painter,
+    onProfileClick: () -> Unit
+) {
+    Box {
+        AnimatedVisibility(
+            visible = !isVisible
+        ) {
+            CircularProgressIndicator()
+        }
+
+        AnimatedVisibility(
+            visible = isVisible,
+        ) {
+            ThumbImageIcon(
+                painter = painter,
+                contentDescription = "Profile",
+                onClick = onProfileClick
+            )
+        }
+    }
 }
---
+++ app/src/main/java/com/github/ephelsa/composefooddelivery/ui/home/HomeViewModel.kt
@@ -0,0 +1,9 @@
+package com.github.ephelsa.composefooddelivery.ui.home
+
+import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel @ViewModelInject constructor(
+
+) : ViewModel() {
+}
